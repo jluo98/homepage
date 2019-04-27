ba969b0ebb8488a7c1e2f56e2c3e019618f038a5
Steve-luo <me@steve-luo.com>
2019-04-27 11:54:40 -0500

    added more detail pages && fixed animation

--- js/detailpage.js
+++ js/detailpage.js
@@ -7,21 +7,28 @@ function showItem(chosenShowItem) {
 
 function openPage() {
 	dim.style.visibility = "visible";
+	page.style.display = "block";
 	dim.classList.toggle('fade');
-	page.style.top = "10vh";
+	setTimeout(showPage, 0);
 }
 
-function hideItem(chosenHideItem) {
-	page = document.getElementById(chosenHideItem);
-	closePage();
+function showPage() {
+	page.style.top = "10vh";
 }
 
 function closePage() {
 	dim.classList.toggle('fade');
-	page.style.top = "120vh";
-	setTimeout(hideDimmer, 500);
+	var all = document.getElementsByClassName('detailedPage');
+	for (var i = 0; i < all.length; i++) {
+		all[i].style.top = '120vh';
+	}
+	setTimeout(hideElement, 500);
 }
 
-function hideDimmer() {
+function hideElement() {
 	dim.style.visibility = "hidden";
+	var all = document.getElementsByClassName('detailedPage');
+	for (var i = 0; i < all.length; i++) {
+		all[i].style.display = 'none';
+	}
 }
