47382e3d8009512abd8e0188b6709cc8f9469e91
Steve-luo <me@steve-luo.com>
2019-04-27 23:24:21 -0500

    added code to stop embedded video when closing panel

--- js/detailpage.js
+++ js/detailpage.js
@@ -8,21 +8,39 @@ function showItem(chosenShowItem) {
 function openPage() {
 	dim.style.visibility = "visible";
 	page.style.display = "block";
-	dim.classList.toggle('fade');
-	setTimeout(showPage, 0);
+	dimmerToggle();
+	setTimeout(showPage, 10);
 }
 
 function showPage() {
 	page.style.top = "10vh";
 }
 
-function closePage() {
+function dimmerToggle() {
 	dim.classList.toggle('fade');
+}
+
+function closePage() {
 	var all = document.getElementsByClassName('detailedPage');
 	for (var i = 0; i < all.length; i++) {
 		all[i].style.top = '120vh';
 	}
+	dimmerToggle();
 	setTimeout(hideElement, 500);
+	setTimeout(pauseVideo, 500);
+	function pauseVideo() {
+		for (var i = 0; i < all.length; i++) {
+			var iframe = all[i].querySelector( 'iframe');
+			var video = all[i].querySelector( 'video' );
+			if ( iframe !== null ) {
+				var iframeSrc = iframe.src;
+			iframe.src = iframeSrc;
+			}
+			if ( video !== null ) {
+				video.pause();
+			}
+		}
+	}
 }
 
 function hideElement() {
